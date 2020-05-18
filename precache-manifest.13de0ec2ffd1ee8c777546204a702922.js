dabb75ffb98e69dfc490d4ca5ddc8978161cf5e9
shangweihung <alannn1230@gmail.com>
2020-05-18 15:42:47 -0700

    Updates

---
+++ precache-manifest.13de0ec2ffd1ee8c777546204a702922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4e83892ee530869d3dfad031968110",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "596556efd9a280df888b",
+    "url": "/todolist-app/static/css/main.44f837ca.chunk.css"
+  },
+  {
+    "revision": "6b008bd99cb5c78bd574",
+    "url": "/todolist-app/static/js/2.3435c7a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist-app/static/js/2.3435c7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596556efd9a280df888b",
+    "url": "/todolist-app/static/js/main.5260e222.chunk.js"
+  },
+  {
+    "revision": "778be53d6838ea43a645",
+    "url": "/todolist-app/static/js/runtime-main.16c65315.js"
+  }
+]);
